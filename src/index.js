fc1cb96788ee21d6efddca339fa859504438a1a5
wtrombone <w.trombone@gmail.com>
2019-01-24 01:48:20 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { combineReducers, createStore } from 'redux';
+
+function productsReducer(state = [], action) {
+    return state;
+}
+
+function userReducer(state = '', action) {
+    return state;
+}
+
+const allReducers = combineReducers({
+    products: productsReducer,
+    user: userReducer
+});
+
+const store = createStore(
+    allReducers, {
+        products: [{ name: 'iPhone' }],
+        user: 'Michael'
+    },
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState())
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
