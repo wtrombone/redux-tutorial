4014d549ac66336ebd7ca8a7787aeb6e106b801f
wtrombone <w.trombone@gmail.com>
2019-01-24 02:29:16 -0500

    added a button to change the state

--- src/App.js
+++ src/App.js
@@ -2,17 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { connect } from 'react-redux';
+import { updateUser } from './actions/user-actions';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onUpdateUser = this.onUpdateUser.bind(this);
+  }
+  
+  onUpdateUser() {
+    this.props.onUpdateUser('Sammy');
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.onUpdateUser}>Update user</p>
+          {this.props.user}
         </header>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  products: state.products,
+  user: state.user
+});
+
+const mapActionsToProps = {
+  onUpdateUser: updateUser
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(App);
--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -1,7 +1,9 @@
+import { UPDATE_USER } from '../actions/user-actions';
+
 export default function userReducer(state = '', { type, payload }) {
     switch (type) {
-        case 'updateUser':
-            return payload;
+        case UPDATE_USER:
+            return payload.user;
         default:
             return state;
     }    
